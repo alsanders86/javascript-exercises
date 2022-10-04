ecbdda1ae284df4696ffd3ffe47e168ff4fd99ea
alsanders86 <alsanders86@gmail.com>
2022-10-04 14:58:05 -0400

    finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, item) {
+    const args = Array.from(arguments);
 
+    // console.log(args[1]);
+    for(j = 1; j < args.length; j++) {
+        for (i = 0; i < array.length; i++) {
+            if(args[j] === array[i]) {
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
