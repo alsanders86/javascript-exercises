be12150fdf61808cba9dd79ddcbcf21e23a919c5
alsanders86 <alsanders86@gmail.com>
2022-10-04 09:48:37 -0400

    finish exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repeatedString = '';
+    if(num >= 0) {
+        for(i = 0; i < num; i++) {
+            repeatedString = repeatedString.concat(string);
+        }
+    } else {
+        repeatedString = "ERROR";
+    }
 
+    return repeatedString;
 };
 
 // Do not edit below this line
