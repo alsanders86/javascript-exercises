79632e5918685b50ffb77a38bb0c5c8b7b5e638e
alsanders86 <alsanders86@gmail.com>
2022-10-04 15:50:15 -0400

    finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let smallNum;
+    let bigNum;
+    let sum = 0;
 
+    if(x < 0 || y < 0) {
+        return "ERROR";
+    }
+
+    if(typeof x !== "number" || typeof y !== "number") {
+        return "ERROR";
+    }
+    
+    if(x > y) {
+        smallNum = y;
+        bigNum = x;
+    } else if (y < x) {
+        smallNum = x;
+        bigNum = y;
+    } else {
+        smallNum = x;
+        bigNum = y
+    }
+
+    const args = Array.from(arguments);
+
+    for(i = smallNum; i <= bigNum; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
