c6b0e6dbb86b3c94270ae6bd5b8d1d51c372b6cb
alsanders86 <alsanders86@gmail.com>
2022-10-04 16:23:08 -0400

    finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
+const ftoc = function(degF) {
+  let degC = (degF - 32) * (5/9);
 
+  let rounded = Math.round(degC * 10) / 10;
+
+  return rounded;
 };
 
-const ctof = function() {
+const ctof = function(degC) {
+  let degF = (degC * 9/5) + 32;
 
+  let rounded = Math.round(degF * 10) / 10;
+
+  return rounded;
 };
 
 // Do not edit below this line
